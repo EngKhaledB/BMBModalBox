8457f19567c853690dae0191adab96cbd10ca238
Khaled B. Abu Alqomboz <eng.khaledb@gmail.com>
2016-12-09 08:43:23 +0200

    Fix the scroll problem

--- assets/js/front.js
+++ assets/js/front.js
@@ -1,10 +1,10 @@
 jQuery(window).bind("load", function () {
     if (window.bmb_box_enabled === true) {
-        jQuery('html').css('overflow', 'hidden !important');
         jQuery('#bmb-modal').fadeIn();
+        jQuery('html').addClass('disableScroll');
         jQuery('#bmb-close').click(function (e) {
             e.preventDefault();
-            jQuery('html,body').css('overflow', '');
+            jQuery('html').removeClass('disableScroll');
             jQuery('#bmb-modal').fadeOut();
         });
     }
